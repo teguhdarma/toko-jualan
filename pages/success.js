0050b0f705e63979296841e6bb709d78b044ffa7
teguhdarma <78896400+teguhdarma@users.noreply.github.com>
2022-05-13 11:00:48 +0900

    forth commit

--- pages/success.js
+++ pages/success.js
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { BsBagCheckFill } from 'react-icons/bs';
 
-import { useStateContext } from '../context/StateContext';
 import { runFireworks } from '../lib/utils';
+import { useStateContext } from '../context/StateContex';
 
 const Success = () => {
   const { setCartItems, setTotalPrice, setTotalQuantities } = useStateContext();
